604ee6ed21cc86b3a336e0d93c5c7bad7f7b57a7
aasbangiftsons <56349780+aasbangiftsons@users.noreply.github.com>
2020-02-18 12:38:08 +0530

    Add files via upload

---
+++ classwithoutinit.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 12:23:04 2020
+
+@author: admin
+"""
+class b:
+    def setname(self,a):
+        self.a=a
+    
+    def getname(self):
+        return self.a
+
+a1=b()
+a1.setname('ozbie')
+print(a1.getname())
